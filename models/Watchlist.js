194982329ced1a7a8f23920539fac03d56623c6d
Joe Curtis <joecurtis9999@gmail.com>
2022-04-06 10:30:32 +0100

    initialized watchlist with title, releasedate, overview, rating

--- models/Watchlist.js
+++ models/Watchlist.js
@@ -4,3 +4,31 @@ const sequelize = require("../config/connection");
 
 class Watchlist extends Model {}
 
+Watchlist.init(
+    {
+        title: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        release_date: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        overview: {
+            type: DataTypes.TEXT,
+            allowNull: false,
+        },
+        rating: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+        },
+    },
+    {
+        sequelize,
+        timestamps: false,
+        freezeTableName: true,
+        modelName: "watchlist"
+    }
+)
+
+module.exports = Watchlist;
