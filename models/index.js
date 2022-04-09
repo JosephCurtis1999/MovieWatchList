337a9f13fb53d5db8ed539785dad032768d9f3af
sikandersultan <sikandersultanmirza@gmail.com>
2022-04-09 13:02:13 +0100

    database

--- models/index.js
+++ models/index.js
@@ -1,9 +1,31 @@
 const Student = require("./Student");
 const Watchlist = require("./Watchlist");
-const Movie = require("./Movie")
+const Movie = require("./Movie");
 
 
+Watchlist.belongsTo(Student, {
+    foreignKey: 'user_id',
+    onDelete: "cascade"
+});
 
+Student.hasOne(Watchlist, {
+    foreignKey: 'user_id',
+    onDelete: "cascade"
+});
 
+Watchlist.hasMany(Movie, {
+    foreignKey: 'movie',
+    onDelete: "cascade"
+});
+
+Movie.belongsTo(Watchlist, {
+    foreignKey: 'movie',
+    onDelete: "cascade"
+});
+
+Movie.belongsTo(Student, {
+    foreignKey: 'movie',
+    onDelete: "cascade"
+});
 
 module.exports = { Student, Watchlist, Movie };
